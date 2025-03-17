cf7744f95b2aa5aaa4a669926a720c0d2644134b
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-17 13:54:37 +0000

    Wip

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,65 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+public static String readfile(String fileName){
+	String contents = "";
+	BufferedReader inputStream = null;
 	
+	try{
+		String line;
+inputStream = new BufferedReader(new FileReader(fileName));
+while(line = inputStream.readLine() != null){
+contents = contents + line;
+}
+	}
 
+	catch(IOException e){
+		System.out.println("IOE exception");
+	}
+	finally{
+		try{
+			if (inputStream != null){
+				inputStream.close();
+			}
+
+		}
+		catch(IOException e){
+			System.out.println("IOE exception");
+		}
+	}
+	return contents;
+}
+	
+public static String backwardsReadfile(String fileName){
+	String contents = "";
+	BufferedReader inputStream = null;
+	
+	try{
+		String line;
+inputStream = new BufferedReader(new FileReader(fileName));
+while(line = inputStream.readLine() != null){
+contents = contents + line; 
+}
+	}
+
+	catch(IOException e){
+		System.out.println("IOE exception");
+	}
+	finally{
+		try{
+			if (inputStream != null){
+				inputStream.close();
+			}
+
+		}
+		catch(IOException e){
+			System.out.println("IOE exception");
+		}
+	}
+String bwd = "";
+	for (int i = contents.length(); i > 0; i--){
+		bwd = bwd + contents.charAt(i);
+	}
+	return bwd;
+}
 }
